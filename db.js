b09dbf16e84f498310c5341375621f1d5922670a
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2019-08-21 10:54:40 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,30 +1,14 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
-var p = "date$"
+var p = "date!"
 
 client.on('ready', () => {
     client.user.setActivity('loading..',{ type: "PLAYING" })
     client.user.setStatus('dnd')
-    setTimeout(status1, 6000)
     console.log('ready launched bot...')
 });
 
-function status1() {
-client.user.setActivity("фигню", {
-  type: "STREAMING",
-  url: "https://www.twitch.tv/monstercat"
-});
-}
 
-client.on('message', message => {
-    if(message.content.startsWith(p + 'test')) {
-    let now = new Date();
-message.channel.send(now)
-console.log(now)
-
-
-  }
-});
 
 client.login(process.env.BOT_SECRET);
