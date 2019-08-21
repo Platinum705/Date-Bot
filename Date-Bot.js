2c63768c39b57a703d909a754e3f670b0d582e05
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2019-08-21 10:42:59 +0500

    Update Date-Bot.js

--- Date-Bot.js
+++ Date-Bot.js
@@ -3,9 +3,24 @@ const fs = require("fs");
 const client = new Discord.Client();
 var p = "date$"
 
+client.on('ready', () => {
+    client.user.setActivity('loading..',{ type: "PLAYING" })
+    client.user.setStatus('dnd')
+    setTimeout(status1, 6000)
+    console.log('ready launched bot...')
+});
+
+function status1() {
+client.user.setActivity("фигню", {
+  type: "STREAMING",
+  url: "https://www.twitch.tv/monstercat"
+});
+}
+
 client.on('message', message => {
     if(message.content.startsWith(p + 'test')) {
-    
+    let now = new Date();
+console.log(now)
 
 
   }
